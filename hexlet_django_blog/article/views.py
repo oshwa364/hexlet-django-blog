28a78f2bf39fbfa4aaae646ca1265fa3684ff573
Artyom Rozhkov <argue364@gmail.com>
2026-04-08 01:51:15 +0200

    add class-view

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from hexlet_django_blog.article.apps import ArticleConfig
+from django.views import View
+from django.http import HttpResponse
 
 
-def index(request):
-    return render(
-        request,
-        "article/index.html",
-        context={
-            "app": ArticleConfig.name,
-        },
-    )
+class IndexView(View):
+    template_name = 'article/index.html'
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data()
+        return render(request, self.template_name, context)
+
+    def get_context_data(self,):
+        context = {}
+        context['app'] = ArticleConfig.name
+        return context
