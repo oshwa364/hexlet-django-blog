68ffffcc1c9df2f2d311d1b01eb1a273b154192c
Artyom Rozhkov <argue364@gmail.com>
2026-04-11 17:40:06 +0200

    please help me

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from hexlet_django_blog.article.models import Article
 
@@ -11,5 +11,17 @@ class IndexView(View):
             "article/index.html",
             context={
                 "articles": articles,
+            },
+        )
+    
+
+class ArticleView(View):
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs["id"])
+        return render(
+            request,
+            "article/detail.html",
+            context={
+                "article": article,
             },
         )
